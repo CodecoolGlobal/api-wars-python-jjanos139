66ce8e906ad34be6b8bc324330ba48f8bec30947
jjanos139 <jdale139@gmail.com>
2022-01-22 19:55:32 +0000

    Create flask server and add generic index.html

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
-# put your code here
+from flask import Flask, session, redirect, url_for, escape, request, render_template, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        debug=True,
+    )
