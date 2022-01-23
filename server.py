965fffa766cfc8dd41b4740290f0c99f4435452b
jjanos139 <jdale139@gmail.com>
2022-01-23 13:13:23 +0000

    Add bootstrap to html and table structure with reference to database.

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template, flash
 
+import data_manager
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    planets = data_manager.get_all_planet_data()
+    return render_template('index.html', planets=planets)
 
 
 if __name__ == '__main__':
