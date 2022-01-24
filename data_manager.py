b0eb8dc0dd71a4d5d3c37960b301da7b0d84c5bb
jjanos139 <jdale139@gmail.com>
2022-01-24 11:34:49 +0000

    Add login and registration routes and html files. Add database connections.

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
 import connection
+import bcrypt
 
 
 @connection.connection_handler
@@ -8,4 +9,43 @@ def get_all_planet_data(cursor):
     """
     cursor.execute(query)
     return cursor.fetchall()
+
+
+def hash_password(plain_text_password):
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+@connection.connection_handler
+def get_users(cursor):
+    query = f"""
+    SELECT * FROM api_users"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_password(cursor, username):
+    query = f"""
+        SELECT password FROM api_users
+        WHERE username = %(username)s
+        """
+    cursor.execute(query, {'username': f"{username}"})
+    return cursor.fetchone()
+
+
+@connection.connection_handler
+def add_user(cursor, username, password):
+    query = f"""
+    INSERT INTO api_users (username, password)
+    VALUES (
+    %(username)s,
+    %(password)s)
+    """
+    cursor.execute(query, {'username': f"{username}", 'password': f"{password}"})
 
--- server.py
+++ server.py
@@ -1,13 +1,59 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template, flash
-
 import data_manager
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 @app.route('/')
 def main():
     planets = data_manager.get_all_planet_data()
     return render_template('index.html', planets=planets)
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users = data_manager.get_users()
+        usernames = []
+        for user in users:
+            usernames.append(user['username'])
+        if username in usernames:
+            return render_template('registration.html', message="Username already in use")
+        data_manager.add_user(username, password)
+        # hashed_password = data_manager.hash_password(password)
+        # user_id = data_manager.add_user(username, hashed_password)
+        # user = data_manager.get_user(user_id)
+        return redirect(url_for('main'))
+    return render_template('registration.html')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users = data_manager.get_users()
+        usernames = []
+        for user in users:
+            usernames.append(user['username'])
+        if username in usernames and password == data_manager.get_password(username)['password']:
+            # hashed_password = data_manager.get_password(session['username'])['password']
+            # if data_manager.verify_password(password, hashed_password):
+            session['username'] = username
+            session['password'] = password
+            return redirect(url_for('main'))
+        else:
+            return render_template('login.html', message='Invalid password or email!')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
