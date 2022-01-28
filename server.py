8f30191d0ca852cf36c16585f215e5fc93690596
jjanos139 <jdale139@gmail.com>
2022-01-28 09:06:17 +0000

    Add heroku files - test.

--- server.py
+++ server.py
@@ -55,10 +55,10 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('main'))
 
-
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True,
-    )
+#
+# if __name__ == '__main__':
+#     app.run(
+#         host='0.0.0.0',
+#         port=5000,
+#         debug=True,
+#     )
